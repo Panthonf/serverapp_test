ec0c49683e70094b67cd80fa30d328b87c21b145
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-06-25 16:17:50 +0700

    Update UserController.ts

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,19 +1,17 @@
 import { Request, Response } from "express";
-import { User, getUsers, getUserById } from "../models/User";
+import { getUsers, getUserById, User } from "../models/User";
 
-export function getAllUsers(req: Request, res: Response): void {
-  getUsers((users: User[]) => {
-    res.json(users);
-  });
+export async function getAllUsers(req: Request, res: Response): Promise<void> {
+  const users: User[] = await getUsers();
+  res.json(users);
 }
 
-export function getUser(req: Request, res: Response): void {
-  const userId: number = parseInt(req.params.id, 10);
-  getUserById(userId, (user: User | undefined) => {
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  });
+export async function getUser(req: Request, res: Response): Promise<void> {
+  const id = parseInt(req.params.id, 10);
+  const user: User | undefined = await getUserById(id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
 }
