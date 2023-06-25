4de58cb1b13f8da77e2f9d154fefd47f27e910b8
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-06-25 16:18:05 +0700

    Update users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,24 +1,9 @@
-import express, { Request, Response } from "express";
-import { getUsers, getUserById } from "../models/User";
+import express from "express";
+import { getAllUsers, getUser } from "../controllers/UserController";
+
 const router = express.Router();
 
-// GET /users
-router.get("/", (req: Request, res: Response) => {
-  getUsers((users: any) => {
-    res.json(users);
-  });
-});
-
-// GET /users/:id
-router.get("/:id", (req: Request, res: Response) => {
-  const userId: number = parseInt(req.params.id, 10);
-  getUserById(userId, (user: any | undefined) => {
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  });
-});
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
 
 export default router;
