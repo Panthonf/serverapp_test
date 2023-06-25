589fcb0a8e119124a601a6f510d57bf3b66eb3c8
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-06-25 16:18:02 +0700

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import connection from "../db";
+import dbPool from "../db";
 
 export interface User {
   id: number;
@@ -6,31 +6,35 @@ export interface User {
   email: string;
 }
 
-export function getUsers(callback: (users: User[]) => void): void {
-  const query = "SELECT * FROM users";
-  connection.query(query, (err: any, rows: any) => {
-    if (err) {
-      console.error("Error retrieving users:", err);
-      callback([]);
-      return;
-    }
-    const users: User[] = rows;
-    callback(users);
-  });
+export async function getUsers(): Promise<User[]> {
+  try {
+    const query = "SELECT * FROM users";
+    const result = await dbPool.query(query);
+    return result.recordset.map((row: any) => ({
+      id: row.id,
+      name: row.name,
+      email: row.email,
+    }));
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    return [];
+  }
 }
 
-export function getUserById(
-  id: number,
-  callback: (user: User | undefined) => void
-): void {
-  const query = "SELECT * FROM users WHERE id = ?";
-  connection.query(query, [id], (err: any, rows: any) => {
-    if (err) {
-      console.error("Error retrieving user:", err);
-      callback(undefined);
-      return;
-    }
-    const user: User | undefined = rows[0];
-    callback(user);
-  });
+export async function getUserById(id: number): Promise<User | undefined> {
+  try {
+    const query = "SELECT * FROM users WHERE id = @id";
+    const result = await dbPool.request().input("id", id).query(query);
+    const row = result.recordset[0];
+    return row
+      ? {
+          id: row.id,
+          name: row.name,
+          email: row.email,
+        }
+      : undefined;
+  } catch (error) {
+    console.error("Error retrieving user:", error);
+    return undefined;
+  }
 }
